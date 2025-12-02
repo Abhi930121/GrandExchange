e3afeb24ae07cd83ff5c153c695aab56b392ba39
Abhi930121 <130537039+Abhi930121@users.noreply.github.com>
2025-12-03 00:43:31 +0530

    Update App.jsx

--- grandexchange/src/App.jsx
+++ grandexchange/src/App.jsx
@@ -1,3 +1,4 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
