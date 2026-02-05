c2021e0809d2d366ed74d836497ee05663ea9e1d
Abhi930121 <130537039+Abhi930121@users.noreply.github.com>
2026-02-05 11:02:28 +0530

    Update GamingPlatform.jsx

--- grandexchange/src/pages/GamingPlatform.jsx
+++ grandexchange/src/pages/GamingPlatform.jsx
@@ -1,15 +1,22 @@
 export default function GamingPlatform() {
   return (
+    <div style={{
+      width: '100vw',
+      height: '100vh',
+      margin: 0,
+      padding: 0,
+      overflow: 'hidden'
+    }}>
       <iframe
         src="https://demo.oriolai.com/ge/"
         title="Gaming Platform"
         style={{
           width: '100%',
-          height: '80vh',
-          border: 'none',
-          borderRadius: '12px'
+          height: '100%',
+          border: 'none'
         }}
         allowFullScreen
       />
+    </div>
   );
 }
